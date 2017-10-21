0b85e334e8720cf57dfda45e7cb4e1b51b07bbd9
Lars Kniep <larskniep@gmail.com>
2017-10-21 17:40:51 +0200

    added more stuff

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,15 @@ module.exports = {
         hid: 'description',
         name: 'description',
         content: 'Turn OFF and see Battery status from up to 4 Wireless XBOX 360 Controllers connected to a PC (simultaneously). Works on almost all versions of Windows, including Windows 10!'
-      }
+      },
+      {property: 'og:type', content: 'article'},
+      {property: 'og:title', content: 'XBOX 360 Controller Manager for PC'},
+      {property: 'og:description', content: 'Turn OFF and see Battery status from up to 4 Wireless XBOX 360 Controllers connected to a PC (simultaneously). Works on almost all versions of Windows, including Windows 10!'}
+      {property: 'og:image', content: 'https://xbox360controller.software/favicon.ico'},
+      {property: 'twitter:card', content: 'summary'},
+      {property: 'twitter:title', content: 'XBOX 360 Controller Manager for PC'},
+      {property: 'twitter:description', content: 'Turn OFF and see Battery status from up to 4 Wireless XBOX 360 Controllers connected to a PC (simultaneously). Works on almost all versions of Windows, including Windows 10!'}
+      {property: 'twitter:image', content: 'https://xbox360controller.software/favicon.ico'}
     ],
     link: [
       {rel: 'canonical', href: 'https://xbox360controller.software'},
@@ -16,7 +24,7 @@ module.exports = {
       {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'}
     ]
   },
-  plugins: ['~plugins/filters.js','~plugins/event-hub.js'],
+  plugins: ['~plugins/filters.js', '~plugins/event-hub.js'],
   modules: [
     '@nuxtjs/vuetify'
   ],
