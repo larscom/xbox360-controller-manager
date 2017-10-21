2ebfe51c166797ea7c9b4b327fcaf57ea270fed7
Lars Kniep <larskniep@gmail.com>
2017-10-21 16:52:47 +0200

    SEO updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   head: {
-    title: 'XBOX 360 Controller Manager',
+    title: 'XBOX 360 Controller Manager for PC',
     meta: [
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui'},
       {
         hid: 'description',
         name: 'description',
-        content: 'XBOX 360 Controller Manager, turn off a wireless XBOX 360 controller for PC. Works on all versions of windows.'
+        content: 'Turn OFF and see Battery status from up to 4 Wireless XBOX 360 Controllers connected to a PC (simultaneously). Works on almost all versions of Windows, including Windows 10!'
       }
     ],
     link: [
+      {rel: 'canonical', href: 'https://xbox360controller.software'},
       {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
       {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'}
     ]
